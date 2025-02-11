57ca83fe50b3f60a8a78b0e8b1118a11cfc00052
Luiz Felipe <luizfelipe.nascente@gmail.com>
2025-02-11 14:49:25 -0300

    coments

--- Services/CreateAccountBankService.cs
+++ Services/CreateAccountBankService.cs
@@ -11,36 +11,43 @@ public class CreateBankAccountService
 {
     public CreateBankAccountService()
     {
-        
+        // contrutor
     }
     public void CreateBankAccount(AccountBankModel accountData)
     {
+        // verifica se accountData tem algum dado vazio
         if(accountData.Name == string.Empty || accountData.Email == string.Empty || accountData.Phone == string.Empty || accountData.Password == string.Empty)
-        {
+        {   
+            //caso tenha algo vazio, retorna, manda para um view de erro
             new CreateBankAccountView().CreateBankAccountFaillView();
             return;
         }
 
+        //adiciona dados que accountData ainda não tem
         accountData.AccountId = new Guid();
         accountData.AccounStatus = AccountStatusEnum.Active;
-        accountData.AccountNumber = BankAccountCodeGenerator();
+        accountData.AccountNumber = BankAccountCodeGenerator(); // chama um metodo para gerar e verificar um novo numero de conta
         accountData.Balance = 0;
         accountData.AddOn = DateTime.Now;
 
+        // instacia a classe de repositorio e manda o accountData com todos os dados para serem salvos
         new CreateBankAccountRepository(accountData);
+        // instanacia a view que mostra o resultado de sucesso na criação de uma nova conta
         new CreateBankAccountView().CreateBankAccountSuccessView(accountData.Name, accountData.AccountNumber);
          
     }
 
     public int BankAccountCodeGenerator()
     {
+        //Gerar um numero aleatorio para definciar como numero da conta corrente
         int bankAccountNumber = new Random().Next(100000, 999999);
-        //var check = new CheckAccountNumberService(bankAccountNumber);
+        // instancia a classe que verifica se esse numero já existe no banco
         if(new CheckAccountNumberService().Check(bankAccountNumber))
-        {
+        {   
+             //Caso exista, chama o metodo novamente para criar um novo numero   
              return BankAccountCodeGenerator();
         }
-        
+        // caso não exista, retorna o nuéro gerado para salvar no banco
         return bankAccountNumber;
        
     }
