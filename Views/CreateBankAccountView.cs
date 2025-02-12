a20fdf78e3a57183f1061fa1f5078e2bbc1b2ff5
Luiz Felipe <luizfelipe.nascente@gmail.com>
2025-02-12 00:15:04 -0300

    refactor CreateBankAccountView

--- Views/CreateBankAccountView.cs
+++ Views/CreateBankAccountView.cs
@@ -11,7 +11,7 @@ public class CreateBankAccountView
     {   
         Console.Clear();
 
-        var welcome = new Panel($"{name} Seja bem vindo ao NewBank! \nO nuúmero da sua conta é {accountNumber} e para iniciar, pressione qulaquier tecla \ne faça login com seu email e senha");
+        var welcome = new Panel($"{name} Seja bem vindo ao NewBank! \nO número da sua conta é {accountNumber}  ^.^  \nPara iniciar, pressione qulaquier tecla e faça login com seu email e senha");
         // Duplica a borda do painel
         welcome.Border = BoxBorder.Double;
         // Escreve o cabeçalho em tela
@@ -24,7 +24,7 @@ public class CreateBankAccountView
     public void CreateBankAccountFaillView()
     {   
 
-        Console.WriteLine("Todos os dados são obrigatorios. Presisona qualquer tecla para continuar");
+        Console.WriteLine("Todos os dados são obrigatórios!!! Presisone qualquer tecla para voltar ao meu inicial");
         // Espara apenas um clique qualquer para em seguida levar para o menu inicial
         Console.ReadKey();
         new MainMenu();
