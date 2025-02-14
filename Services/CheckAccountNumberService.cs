adc20319fd000247572605e26170e2d63f26644b
Luiz Felipe <luizfelipe.nascente@gmail.com>
2025-02-14 14:07:11 -0300

    refactoring +

--- Services/CheckAccountNumberService.cs
+++ Services/CheckAccountNumberService.cs
@@ -5,9 +5,11 @@ namespace MyNewBank.Services;
 
 public class CheckAccountNumberService
 {   
+    CheckAccountNumberRepository checkAccountNumberRepository;
     public CheckAccountNumberService()
     {
         // construtor para injeção de dependencia
+        checkAccountNumberRepository = new CheckAccountNumberRepository();
     }
     
     //Metodo que enviar o codigo da conta para o repositorio verificar
@@ -16,7 +18,7 @@ public class CheckAccountNumberService
     {
         // o retorno já é a chamda no check pois ele ttbm é bool e já retorna 
         // true or false
-        return new CheckAccountNumberRepository().Check(bankAccountNumber);
+        return checkAccountNumberRepository.Check(bankAccountNumber);
        
     }
 }
